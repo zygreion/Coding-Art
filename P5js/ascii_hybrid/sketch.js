da7913713f38547d9295d2b2b545c14cb7b0d171
Zumi <akumuis1@gmail.com>
2022-08-11 06:05:17 +0700

    Big Re-organize
    - changing asciiHybrid.js to sketch.js
    - colorize small optimization
    - adding input file and webcam button

---
+++ P5js/ascii_hybrid/sketch.js
@@ -0,0 +1,179 @@
+const chars = 'Ñ@#W$9876543210?!abc;:+=-,._                    ';
+let savedFrameTime = 0;
+let isStopped = false;
+let counter = 0;
+let init_vw, init_vh;
+let initializeEnd;
+
+// Customization
+let walk = false;
+let cameraMode = false;
+let colorize = false;
+
+let video, vidRef, c, type, lastAvg;
+
+function preload() {
+  type = 'video';
+  video = createVideo('bird.mp4', initialize);
+  
+  densitySlider.value = '100';
+  widthSlider.value = window.innerWidth * 3/4;
+  widthSlider.max = window.outerWidth;
+}
+
+function draw() {
+  if (!video) return;
+  if (type !== 'image' && !video.loadedmetadata) return;
+  if (initializeEnd) {
+    if (typeof(c) === 'undefined') {
+      c = createCanvas(int(56 * init_vw / init_vh), 56);
+      c.parent(videoDisplay);
+      initializeEnd = false;
+    }
+  }
+  if (c.width !== 56 * init_vw / init_vh || c.height !== 56) c.resize(56 * init_vw / init_vh, 56);
+  
+  if (type === 'image') c.drawingContext.drawImage(vidRef, 0, 0, c.width, c.height);
+  if (type === 'video' || type === 'camera') image(video, 0,0, c.width, c.height);
+  
+  video.loadPixels();
+  let samePixels = 0;
+  
+  let isSavingTime = false
+  if (second() % 2 === 0 && savedFrameTime !== second()) {
+    savedFrameTime = second();
+    isSavingTime = true;
+  }
+  
+  let asciiImage = '';
+  for (let y = 0; y < video.height; y++) {
+    lastAvg = -1;
+    for (let x = 
+         (!cameraMode) ? 0 : video.width-1; 
+         (!cameraMode) ? x < video.width : x >= 0; 
+         (!cameraMode) ? x++ : x--) {
+      const pixIndex = (x + y * video.width) * 4;
+      const r = video.pixels[pixIndex];
+      const g = video.pixels[pixIndex+1];
+      const b = video.pixels[pixIndex+2];
+      
+      if (savedFrame.pixels[pixIndex] === r &&
+          savedFrame.pixels[pixIndex+1] === g &&
+          savedFrame.pixels[pixIndex+2] === b) samePixels++;
+      
+      if (!isStopped && isSavingTime) {
+        savedFrame.pixels[pixIndex] = r;
+        savedFrame.pixels[pixIndex+1] = g;
+        savedFrame.pixels[pixIndex+2] = b;
+      }
+      
+      const avg = (r+g+b) /3;
+      const charIndex = map(avg, 0, 255, chars.length, 0);
+      let c = chars.charAt((!walk) ? charIndex : (charIndex + counter/6) % chars.length);
+      if (c === ' ') (c = (!colorize) ? '&nbsp' : '^');
+      if (!colorize) asciiImage += c;
+      else {
+        if (lastAvg !== avg) {
+          if (x > 0) asciiImage += '</span>';
+          asciiImage += `<span style="color: rgb(${r},${g},${b})">${c}`;
+        } else {
+          asciiImage += c;
+        }
+        // asciiImage += `<span style="color: rgb(${r},${g},${b})">${c}</span>`;
+      }
+      lastAvg = avg;
+    }
+    asciiImage += '<br>';
+  }
+  
+  if (!isStopped) {
+    asciiCanvas.innerHTML = asciiImage; 
+    counter++;
+  }
+  
+  if (type === 'image') return;
+  let allow = 0;
+  (samePixels === video.width * video.height) ? allow++ : allow--;
+  (!isStopped) ? allow++ : allow--;
+  switch (allow) {
+    case 2:
+      isStopped = true;
+      asciiCanvas.style.opacity = 0.4;
+      dcDiv.style.opacity = 1;
+      break;
+    case -2:
+      isStopped = false;
+      asciiCanvas.style.opacity = 1;
+      dcDiv.style.opacity = 0;
+      break;
+  }
+}
+
+function inputLoad(param) {
+  let t = (param !== 'camera') ? param.type.slice(0, 5) : param;
+  if (t !== 'video' && t !== 'camera' && t !== 'image') {
+    fileInput.classList.remove('fileInputActive');
+    return;
+  }
+  
+  let lastType = type;
+  type = t;
+  let blob = (param !== 'camera') ? URL.createObjectURL(param) : (0);
+  
+  (type === 'camera') ? 
+    ((lastType !== type) ? 
+       (fileInput.value = '', 
+       webcamButton.classList.add('webcamButtonActive'), 
+       fileInput.classList.remove('fileInputActive'), 
+       video = createCapture(VIDEO, initialize)) : (0)) :
+    ((type === 'video' || type === 'image') ? 
+      (fileInput.classList.add('fileInputActive'),
+      webcamButton.classList.remove('webcamButtonActive'),
+      (type === 'video') ? 
+       video = createVideo(blob, initialize) : 
+       (video = loadImage(blob, initialize), 
+        vidRef = new Image(), 
+        vidRef.src = blob)) : (0)
+    );
+  
+  // if (type === 'camera') {
+  //   if (lastType === type) return;
+  //   fileInput.value = '';
+  //   webcamButton.classList.add('webcamButtonActive');
+  //   fileInput.classList.remove('fileInputActive');
+  //   video = createCapture(VIDEO, initialize);
+  // } else if (type === 'video' || type === 'image') {
+  //   fileInput.classList.add('fileInputActive');
+  //   webcamButton.classList.remove('webcamButtonActive');
+  //   (type === 'video') ? video = createVideo(blob, initialize) : 
+  //     (video = loadImage(blob, initialize), vidRef = new Image(), vidRef.src = blob);
+  // }
+}
+
+function initialize() {
+  initializeEnd = false;
+  counter = 0;
+  if (type === 'video' || type === 'camera') 
+    (video.hide(), video.volume(0), video.play(), video.loop())
+  init_vw = video.width; init_vh = video.height;
+  changeWidth(); changeDensity();
+  initializeEnd = true;
+}
+
+function changeWidth() {
+  changeFontSize();
+}
+
+function changeDensity() {
+  let newW = int(densitySlider.value);
+  let newH = int(newW / init_vw * init_vh);
+  (type === 'video' || type === 'camera') ? video.size(newW, newH) : (video = loadImage(vidRef.src, () => video.resize(newW, newH)));
+  savedFrame = createImage(newW, newH);
+  changeFontSize();
+}
+
+function changeFontSize() {
+  // at widthSlider.value === 100, 
+  // the asciiCanvas fontSize is 16px
+  asciiCanvas.style.fontSize = 16 * int(widthSlider.value)/1320 * 100/int(densitySlider.value) + 'px';
+}
