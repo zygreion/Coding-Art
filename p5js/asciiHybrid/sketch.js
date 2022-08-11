af95682c3eeb64aa08fa4ca954e6dad145d2cff9
Zumi <akumuis1@gmail.com>
2022-08-12 01:48:18 +0700

    Update sketch.js

--- p5js/asciiHybrid/sketch.js
+++ p5js/asciiHybrid/sketch.js
@@ -176,5 +176,5 @@ function changeFontSize() {
 }
 
 function mouseMoved() {
-  if (video.elt.paused && video.volume() === 0) (video.volume(1), video.play());
+  if (video.elt.paused && video.volume() === 0) (video.elt.volume(1), video.elt.play());
 }
