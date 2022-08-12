0efcc6ee64155dd785cfa7af1403c4f3bee11c9f
Zumi <45333659+zygreion@users.noreply.github.com>
2022-08-12 13:50:36 +0700

    Update sketch.js

--- p5js/asciiHybrid/sketch.js
+++ p5js/asciiHybrid/sketch.js
@@ -18,7 +18,7 @@ function preload() {
   
   densitySlider.value = '100';
   widthSlider.value = window.innerWidth * 3/4;
-  widthSlider.max = window.outerWidth-40;
+  widthSlider.max = window.outerWidth;
 }
 
 function draw() {  
